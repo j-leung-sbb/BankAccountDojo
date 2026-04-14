983d485bcb1dc1a93879a6a86b41eff70b7036fd
Jeffrey Leung <J.Leung@s-bb.nl>
2026-04-14 14:35:09 +0200

    Add project files.

---
+++ BankAccount.Tests/Test1.cs
@@ -0,0 +1,47 @@
+﻿namespace BankAccount.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void NewAccount_ShouldHaveZeroBalance()
+        {
+            var bankAccount = new BankAccount();
+            Assert.AreEqual(0m, bankAccount.GetBalance());
+        }
+
+        [TestMethod]
+        public void Deposit_ShouldIncreaseBalance()
+        {
+            var bankAccount = new BankAccount();
+            bankAccount.Deposit(100m);
+            Assert.AreEqual(100m, bankAccount.GetBalance());
+        }
+
+        [TestMethod]
+        public void MultipleDeposits_ShouldAccumulateBalance()
+        {
+            var bankAccount = new BankAccount();
+            bankAccount.Deposit(100m);
+            bankAccount.Deposit(200m);
+            Assert.AreEqual(300m, bankAccount.GetBalance());
+        }
+
+        [TestMethod]
+        public void Withdraw_ShouldDecreaseBalance()
+        {
+            var bankAccount = new BankAccount();
+            bankAccount.Deposit(200m);
+            bankAccount.Withdraw(100m);
+            Assert.AreEqual(100m, bankAccount.GetBalance());
+        }
+
+        [TestMethod]
+        public void WithdrawMoreThanBalance_ShouldThrowException()
+        {
+            var bankAccount = new BankAccount();
+
+            Assert.Throws<InvalidOperationException>(() => bankAccount.Withdraw(50m));
+        }
+    }
+}
---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace BankAccount
+{
+    public class BankAccount
+    {
+        private decimal _balance;
+
+        public void Deposit(decimal amount)
+        {
+            _balance += amount;
+        }
+
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > _balance)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _balance -= amount;
+        }
+    }
+}
